ee4e81d58b121c67cf16ac363a2a11a10e34e798
Gede Indra Adi Brata <indrabrata599@gmail.com>
2023-03-27 10:43:57 +0700

    add array-data-type

---
+++ basic-php/array-data-type.php
@@ -0,0 +1,70 @@
+<?php
+//array in php is dynamic (can add more size)
+//array in php can filled by multi data type
+
+//simple array
+$values = array(1, 2, 3, 4, 5);
+var_dump($values); //checking array
+
+$sports = ["football", "badminton", "swimming"];
+var_dump($sports); //checking array
+
+$combination = ["Indra", 15, 7.8];
+var_dump($combination); //checking array
+
+//array operation
+
+//access value in array using index
+echo "access value in array using index : ";
+echo $values[0];
+echo "\n";
+
+//changing value in array using index
+$sports[0] = "volleyball";
+echo "changing value in array using index : ";
+echo $sports[0];
+echo "\n";
+
+//counting all data array
+$sizeValues = count($values);
+echo "counting all data array : ";
+echo $sizeValues;
+echo "\n";
+
+//adding data in back order (posisi paling belakang)
+$values[] = 12345;
+echo "adding data in back order (last order) : ";
+echo $values[5];
+echo "\n";
+
+//deleting array and index "unset($array[index])"
+unset($combination[0]);
+echo $combination[0]; //return error
+
+//array as a map (assosation between key and value)
+$hobbies = array(
+   "Indra" => "football",
+   "Javier" => "coding",
+   "Fauzi" => "sleep",
+);
+var_dump($hobbies);
+
+$identity = [
+   "NIM" => "225150200xxxxxx",
+   "Name" => "Gede Indra Adi Brata",
+   "Age" => 19,
+   "Address" => [
+      "City" => "Malang",
+      "Country" => "Indonesia"
+   ],
+];
+var_dump($identity);
+
+//get data from map
+echo "Indra's hobby : ";
+echo $hobbies["Indra"];
+echo "\n";
+echo "Indra's city  : ";
+echo $identity["Address"]["City"];
+
+?>
