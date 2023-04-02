b7c72e1910e2dcfb7a91a384348cc9d97c28d018
Gede Indra Adi Brata <indrabrata599@gmail.com>
2023-04-02 12:07:46 +0700

    add function

---
+++ basic-php/function-argument.php
@@ -0,0 +1,10 @@
+<?php
+
+//default value argument
+function sayHello($name = "Anonymous"){
+   echo "hi $name" . PHP_EOL;
+}
+
+
+sayHello();
+?>
---
+++ basic-php/function.php
@@ -0,0 +1,12 @@
+<?php
+// function in php really flexible because can be declared outside of class
+// at least it has been executed before
+
+
+sayHello();
+
+function sayHello(){
+   echo "Hello World!" . PHP_EOL;
+}
+
+?>
